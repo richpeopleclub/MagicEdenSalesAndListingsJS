8645fe3f0cb24fef9290d2d65dcef2b36469a5b3
Ryan <ryan@trat.io>
2022-06-03 21:15:38 -0700

    added try catches for extraneous transactions

--- example/example.js
+++ example/example.js
@@ -1,7 +1,7 @@
-var MESales = require("../index.js")
+var MESales = require("../index")
 
 var tracker = MESales.track(JSON.parse(require("fs").readFileSync("./mints.json", "utf8")), {
-    connectUrl: "https://solana-mainnet.trat.dev/79fbb70b70822e62f4e7b4401638b59a8c7413f4/",
+    connectUrl: "", // your api endpoint
     interval: 5 // 5 seconds between each check
 }); 
 
--- transactionParser.js
+++ transactionParser.js
@@ -25,291 +25,333 @@ var transactionParser = async function (connection, signature) {
 };
 
 function parseCancelBidMagicEdenv2(transaction, signature) {
-  if (!transaction.meta.logMessages.length == 4) {
+  try {
+    if (!transaction.meta.logMessages.length == 4) {
+      return null;
+    }
+    if (
+      !transaction.meta.logMessages[0] ==
+      "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
+    ) {
+      return null;
+    }
+    if (
+      !(
+        transaction.meta.logMessages[1] == "Program log: Instruction: CancelBuy"
+      )
+    ) {
+      return null;
+    }
+
+    if (
+      !transaction.transaction.instructions[0].programId.toBase58() ==
+      "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+    ) {
+      return null;
+    }
+
+    var bidderPub = transaction.transaction.instructions[0].keys[0];
+
+    var mint = transaction.transaction.instructions[0].keys[2];
+
+    var returned = {
+      type: "cancelBid",
+      name: "Cancel Bid",
+      marketplace: "Magic Eden",
+      data: {
+        bidder: bidderPub.pubkey.toBase58(),
+        extras: "v2",
+      },
+      mint: mint.pubkey.toBase58(),
+      time: transaction.blockTime * 1000,
+      signature: signature,
+    };
+    return returned;
+  } catch (e) {
+    //console.log(signature)
     return null;
   }
-  if (
-    !transaction.meta.logMessages[0] ==
-    "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
-  ) {
-    return null;
-  }
-  if (
-    !(transaction.meta.logMessages[1] == "Program log: Instruction: CancelBuy")
-  ) {
-    return null;
-  }
-
-  if (
-    !transaction.transaction.instructions[0].programId.toBase58() ==
-    "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-  ) {
-    return null;
-  }
-
-  var bidderPub = transaction.transaction.instructions[0].keys[0];
-
-  var mint = transaction.transaction.instructions[0].keys[2];
-
-  var returned = {
-    type: "cancelBid",
-    name: "Cancel Bid",
-    marketplace: "Magic Eden",
-    data: {
-      bidder: bidderPub.pubkey.toBase58(),
-      extras: "v2",
-    },
-    mint: mint.pubkey.toBase58(),
-    time: transaction.blockTime * 1000,
-    signature: signature,
-  };
-  return returned;
 }
 
 function parseBidOnMagicEdenv2_2(transaction, signature) {
-  if (!(transaction.meta.logMessages.length == 5)) {
+  try {
+    if (!(transaction.meta.logMessages.length == 5)) {
+      return null;
+    }
+    if (
+      !transaction.meta.logMessages[0] ==
+      "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
+    ) {
+      return null;
+    }
+    if (!(transaction.meta.logMessages[1] == "Program log: Instruction: Buy")) {
+      return null;
+    }
+
+    if (
+      !transaction.transaction.instructions[0].programId.toBase58() ==
+      "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+    ) {
+      return null;
+    }
+
+    var bidderPub = transaction.transaction.instructions[0].keys[0];
+
+    var mint = transaction.transaction.instructions[0].keys[2];
+    var priceLamports = JSON.parse(
+      transaction.meta.logMessages[2].replace("Program log: ", "")
+    ).price;
+
+    var returned = {
+      type: "bid",
+      name: "Bid",
+      marketplace: "Magic Eden",
+      data: {
+        bidder: bidderPub.pubkey.toBase58(),
+        price: priceLamports,
+        extras: "v2 type 2",
+      },
+      mint: mint.pubkey.toBase58(),
+      time: transaction.blockTime * 1000,
+      signature: signature,
+    };
+    return returned;
+  } catch (e) {
+    //console.log(signature)
     return null;
   }
-  if (
-    !transaction.meta.logMessages[0] ==
-    "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
-  ) {
-    return null;
-  }
-  if (!(transaction.meta.logMessages[1] == "Program log: Instruction: Buy")) {
-    return null;
-  }
-
-  if (
-    !transaction.transaction.instructions[0].programId.toBase58() ==
-    "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-  ) {
-    return null;
-  }
-
-  var bidderPub = transaction.transaction.instructions[0].keys[0];
-
-  var mint = transaction.transaction.instructions[0].keys[2];
-  var priceLamports = JSON.parse(
-    transaction.meta.logMessages[2].replace("Program log: ", "")
-  ).price;
-
-  var returned = {
-    type: "bid",
-    name: "Bid",
-    marketplace: "Magic Eden",
-    data: {
-      bidder: bidderPub.pubkey.toBase58(),
-      price: priceLamports,
-      extras: "v2 type 2",
-    },
-    mint: mint.pubkey.toBase58(),
-    time: transaction.blockTime * 1000,
-    signature: signature,
-  };
-  return returned;
 }
 function parseBidOnMagicEdenv2(transaction, signature) {
-  if (!(transaction.meta.logMessages.length == 7)) {
+  try {
+    if (!(transaction.meta.logMessages.length == 7)) {
+      return null;
+    }
+    if (
+      !transaction.meta.logMessages[0] ==
+      "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
+    ) {
+      return null;
+    }
+    if (!(transaction.meta.logMessages[1] == "Program log: Instruction: Buy")) {
+      return null;
+    }
+
+    if (
+      !transaction.transaction.instructions[0].programId.toBase58() ==
+      "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+    ) {
+      return null;
+    }
+
+    var bidderPub = transaction.transaction.instructions[0].keys[0];
+
+    var mint = transaction.transaction.instructions[0].keys[2];
+    var priceLamports = JSON.parse(
+      transaction.meta.logMessages[4].replace("Program log: ", "")
+    ).price;
+
+    var returned = {
+      type: "bid",
+      name: "Bid",
+      marketplace: "Magic Eden",
+      data: {
+        bidder: bidderPub.pubkey.toBase58(),
+        price: priceLamports,
+        extras: "v2",
+      },
+      mint: mint.pubkey.toBase58(),
+      time: transaction.blockTime * 1000,
+      signature: signature,
+    };
+    return returned;
+  } catch (e) {
+    //console.log(signature)
     return null;
   }
-  if (
-    !transaction.meta.logMessages[0] ==
-    "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
-  ) {
-    return null;
-  }
-  if (!(transaction.meta.logMessages[1] == "Program log: Instruction: Buy")) {
-    return null;
-  }
-
-  if (
-    !transaction.transaction.instructions[0].programId.toBase58() ==
-    "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-  ) {
-    return null;
-  }
-
-  var bidderPub = transaction.transaction.instructions[0].keys[0];
-
-  var mint = transaction.transaction.instructions[0].keys[2];
-  var priceLamports = JSON.parse(
-    transaction.meta.logMessages[4].replace("Program log: ", "")
-  ).price;
-
-  var returned = {
-    type: "bid",
-    name: "Bid",
-    marketplace: "Magic Eden",
-    data: {
-      bidder: bidderPub.pubkey.toBase58(),
-      price: priceLamports,
-      extras: "v2",
-    },
-    mint: mint.pubkey.toBase58(),
-    time: transaction.blockTime * 1000,
-    signature: signature,
-  };
-  return returned;
 }
 
 function parseListOnMagicEdenV1(transaction, signature) {
-  if (
-    !transaction.meta.logMessages[0] ==
-    "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
-  ) {
-    return null;
-  }
-  if (!(transaction.meta.logMessages[1] == "Program log: Instruction: Sell")) {
-    return null;
-  }
-  if (
-    !transaction.transaction.instructions[0].programId.toBase58() ==
-    "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-  ) {
-    return null;
-  }
-  if (!(transaction.meta.logMessages.length == 11)) {
-    return null;
-  }
+  try {
+    if (
+      !transaction.meta.logMessages[0] ==
+      "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
+    ) {
+      return null;
+    }
+    if (
+      !(transaction.meta.logMessages[1] == "Program log: Instruction: Sell")
+    ) {
+      return null;
+    }
+    if (
+      !transaction.transaction.instructions[0].programId.toBase58() ==
+      "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+    ) {
+      return null;
+    }
+    if (!(transaction.meta.logMessages.length == 11)) {
+      return null;
+    }
 
-  var sellerPub = transaction.transaction.instructions[0].keys[0];
-  var mint = transaction.transaction.instructions[0].keys[4];
-  var priceLamports = JSON.parse(
-    transaction.meta.logMessages[8].replace("Program log: ", "")
-  ).price;
-  var returned = {
-    type: "list",
-    name: "List",
-    marketplace: "Magic Eden",
-    data: {
-      seller: sellerPub.pubkey.toBase58(),
-      price: priceLamports,
-      extras: "v1",
-    },
-    mint: mint.pubkey.toBase58(),
-    time: transaction.blockTime * 1000,
-  };
-  return returned;
+    var sellerPub = transaction.transaction.instructions[0].keys[0];
+    var mint = transaction.transaction.instructions[0].keys[4];
+    var priceLamports = JSON.parse(
+      transaction.meta.logMessages[8].replace("Program log: ", "")
+    ).price;
+    var returned = {
+      type: "list",
+      name: "List",
+      marketplace: "Magic Eden",
+      data: {
+        seller: sellerPub.pubkey.toBase58(),
+        price: priceLamports,
+        extras: "v1",
+      },
+      mint: mint.pubkey.toBase58(),
+      time: transaction.blockTime * 1000,
+    };
+    return returned;
+  } catch (e) {
+    //console.log(signature)
+    return null;
+  }
 }
 function parseListOnMagicEdenV2(transaction, signature) {
-  if (
-    !transaction.meta.logMessages[0] ==
-    "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
-  )
-    return null;
-  if (!(transaction.meta.logMessages[1] == "Program log: Instruction: Sell"))
-    return null;
+  try {
+    if (
+      !transaction.meta.logMessages[0] ==
+      "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
+    )
+      return null;
+    if (!(transaction.meta.logMessages[1] == "Program log: Instruction: Sell"))
+      return null;
 
-  if (
-    !transaction.transaction.instructions[0].programId.toBase58() ==
-    "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-  )
-    return null;
-  if (!(transaction.meta.logMessages.length == 5)) return null;
+    if (
+      !transaction.transaction.instructions[0].programId.toBase58() ==
+      "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+    )
+      return null;
+    if (!(transaction.meta.logMessages.length == 5)) return null;
 
-  var sellerPub = transaction.transaction.instructions[0].keys[0];
-  var mint = transaction.transaction.instructions[0].keys[4];
-  var priceLamports = JSON.parse(
-    transaction.meta.logMessages[2].replace("Program log: ", "")
-  ).price;
-  var returned = {
-    type: "list",
-    name: "List",
-    marketplace: "Magic Eden",
-    data: {
-      seller: sellerPub.pubkey.toBase58(),
-      extras: "v2",
-      price: priceLamports,
-    },
-    mint: mint.pubkey.toBase58(),
-    time: transaction.blockTime * 1000,
-    signature: signature,
-  };
-  return returned;
+    var sellerPub = transaction.transaction.instructions[0].keys[0];
+    var mint = transaction.transaction.instructions[0].keys[4];
+    var priceLamports = JSON.parse(
+      transaction.meta.logMessages[2].replace("Program log: ", "")
+    ).price;
+    var returned = {
+      type: "list",
+      name: "List",
+      marketplace: "Magic Eden",
+      data: {
+        seller: sellerPub.pubkey.toBase58(),
+        extras: "v2",
+        price: priceLamports,
+      },
+      mint: mint.pubkey.toBase58(),
+      time: transaction.blockTime * 1000,
+      signature: signature,
+    };
+    return returned;
+  } catch (e) {
+    //console.log(signature)
+    return null;
+  }
 }
 
 function parseCancelListOnMagicEden(transaction, signature) {
-  if (
-    !transaction.meta.logMessages[0] ==
-    "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
-  ) {
-    return null;
-  }
-  if (
-    !(transaction.meta.logMessages[1] == "Program log: Instruction: CancelSell")
-  ) {
-    return null;
-  }
+  try {
+    if (
+      !transaction.meta.logMessages[0] ==
+      "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
+    ) {
+      return null;
+    }
+    if (
+      !(
+        transaction.meta.logMessages[1] ==
+        "Program log: Instruction: CancelSell"
+      )
+    ) {
+      return null;
+    }
 
-  if (
-    !transaction.transaction.instructions[0].programId.toBase58() ==
-    "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-  ) {
+    if (
+      !transaction.transaction.instructions[0].programId.toBase58() ==
+      "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+    ) {
+      return null;
+    }
+    var sellerPub = transaction.transaction.instructions[0].keys[0];
+    var mint = transaction.transaction.instructions[0].keys[3];
+    var returned = {
+      type: "cancelList",
+      name: "Cancel Listing",
+      marketplace: "Magic Eden",
+      data: {
+        seller: sellerPub.pubkey.toBase58(),
+      },
+      mint: mint.pubkey.toBase58(),
+      time: transaction.blockTime * 1000,
+      signature: signature,
+    };
+    return returned;
+  } catch (e) {
+    //console.log(signature)
     return null;
   }
-  var sellerPub = transaction.transaction.instructions[0].keys[0];
-  var mint = transaction.transaction.instructions[0].keys[3];
-  var returned = {
-    type: "cancelList",
-    name: "Cancel Listing",
-    marketplace: "Magic Eden",
-    data: {
-      seller: sellerPub.pubkey.toBase58(),
-    },
-    mint: mint.pubkey.toBase58(),
-    time: transaction.blockTime * 1000,
-    signature: signature,
-  };
-  return returned;
 }
 
 function parseBuyOnMagicEdenV2(transaction, signature) {
-  if (
-    !transaction.meta.logMessages[0] ==
-    "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
-  ) {
+  try {
+    if (
+      !transaction.meta.logMessages[0] ==
+      "Program M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K invoke [1]"
+    ) {
+      return null;
+    }
+    if (
+      !(
+        transaction.meta.logMessages[14] ==
+        "Program log: Instruction: ExecuteSale"
+      )
+    ) {
+      return null;
+    }
+
+    if (
+      !transaction.transaction.instructions[0].programId.toBase58() ==
+      "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
+    ) {
+      return null;
+    }
+
+    var sellerPub = transaction.transaction.instructions[2].keys[1];
+    var buyerPub = transaction.transaction.instructions[2].keys[0];
+
+    var mint = transaction.transaction.instructions[2].keys[4];
+    var priceLamports = JSON.parse(
+      transaction.meta.logMessages[10].replace("Program log: ", "")
+    ).price;
+
+    var returned = {
+      type: "buy",
+      name: "buy",
+      marketplace: "Magic Eden",
+      data: {
+        seller: sellerPub.pubkey.toBase58(),
+        buyer: buyerPub.pubkey.toBase58(),
+        extras: "v2",
+        price: priceLamports,
+      },
+      mint: mint.pubkey.toBase58(),
+      time: transaction.blockTime * 1000,
+      signature: signature,
+    };
+    return returned;
+  } catch (e) {
+    //console.log(signature)
     return null;
   }
-  if (
-    !(
-      transaction.meta.logMessages[14] ==
-      "Program log: Instruction: ExecuteSale"
-    )
-  ) {
-    return null;
-  }
-
-  if (
-    !transaction.transaction.instructions[0].programId.toBase58() ==
-    "M2mx93ekt1fmXSVkTrUL9xVFHkmME8HTUi5Cyc5aF7K"
-  ) {
-    return null;
-  }
-
-  var sellerPub = transaction.transaction.instructions[2].keys[1];
-  var buyerPub = transaction.transaction.instructions[2].keys[0];
-
-  var mint = transaction.transaction.instructions[2].keys[4];
-  var priceLamports = JSON.parse(
-    transaction.meta.logMessages[10].replace("Program log: ", "")
-  ).price;
-
-  var returned = {
-    type: "buy",
-    name: "buy",
-    marketplace: "Magic Eden",
-    data: {
-      seller: sellerPub.pubkey.toBase58(),
-      buyer: buyerPub.pubkey.toBase58(),
-      extras: "v2",
-      price: priceLamports,
-    },
-    mint: mint.pubkey.toBase58(),
-    time: transaction.blockTime * 1000,
-    signature: signature,
-  };
-  return returned;
 }
 
 module.exports = transactionParser;
